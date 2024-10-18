951fc5eada84b04775dc9480cede054d0f9e0e33
cjdelacruz <cjdelacruz0521@gmail.com>
2024-10-18 07:21:33 +0700

    initial commit with arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,13 @@
+// Create an array called favSingers and store your three favorite singers in it
+
+const favSingers = ["Billie eilish", "Micheal Jackson", "Bruno Mars" ];
+console.log(favSingers[0]);
+
+const favNumbers = [5,9,11,19,21];
+
+const mixedArr =  ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
